98c6f1dfb8877077b6a5a1bcc306506259c569ed
cervs257 <cervantes257@gmail.com>
2023-12-30 11:35:01 -0500

    lifetimes

--- rust_specialization_coursera/lifetimes/src/main.rs
+++ rust_specialization_coursera/lifetimes/src/main.rs
@@ -1,4 +1,10 @@
-// so Rust knows that the lifetime of the return value is the same as the lifetime of the vector
+// There may be multiple lifetimes. When that occurs,
+// annotate the lifetimes to help the compiler understand which lifetime it will use to ensure the references are valid at runtime.
+// The borrow checker can't determine if the reference will be a valid one either.
+// It doesn't know how the input parameters' lifetime relates to the return value's lifetime.
+//This ambiguity is why we need to annotate the lifetimes manually.
+
+// Rust doesn't know if the return value will be a reference to the vector or a reference to the value parameter => annotate lifetimes manually
 fn copy_and_return<'lifetime>(
     vector: &'lifetime mut Vec<String>,
     value: &'lifetime str,
