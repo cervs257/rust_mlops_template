55167eebda2c9e90099b767ed50347d5ed5553c7
cervs257 <cervantes257@gmail.com>
2024-01-07 21:20:44 -0500

    fundamentals week4

---
+++ rust_specialization_coursera/fundamentals_week4/tests/test_colors.rs
@@ -0,0 +1,12 @@
+use fundamentals_week4::colors::{Color, ColorString};
+
+#[test]
+fn test_red_coloring() {
+    let mut color_string = ColorString {
+        color: Color::Red,
+        string: "Red".to_string(),
+        colorized: "".to_string(),
+    };
+    color_string.paint();
+    assert_eq!(color_string.colorized, "\x1b[31mRed\x1b[0m");
+}
